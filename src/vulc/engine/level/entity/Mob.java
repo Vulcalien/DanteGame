b9ccde96bf721f986af8338c9c7d361844b7116d
Vulcalien <vulcalien+github@gmail.com>
2021-03-18 12:08:59 +0100

    Player's move animation

--- src/vulc/engine/level/entity/Mob.java
+++ src/vulc/engine/level/entity/Mob.java
@@ -2,17 +2,23 @@ package vulc.engine.level.entity;
 
 public class Mob extends Entity {
 
-	public int dir;
+	public int dir = 2;
+	public int moveAnimation = 0;
 
 	public boolean[] move(int xm, int ym) {
 		boolean[] result = super.move(xm, ym);
 
-		if(xm < 0) dir = 1;
-		else if(xm > 0) dir = 3;
+		if(xm != 0 || ym != 0) {
+			if(xm < 0) dir = 1;
+			else if(xm > 0) dir = 3;
 
-		if(ym < 0) dir = 0;
-		else if(ym > 0) dir = 2;
+			if(ym < 0) dir = 0;
+			else if(ym > 0) dir = 2;
 
+			moveAnimation++;
+		} else {
+			moveAnimation = 0;
+		}
 		return result;
 	}
 
--- src/vulc/engine/level/entity/Player.java
+++ src/vulc/engine/level/entity/Player.java
@@ -1,14 +1,16 @@
 package vulc.engine.level.entity;
 
+import vulc.bitmap.Bitmap;
 import vulc.bitmap.IntBitmap;
 import vulc.engine.gfx.Screen;
+import vulc.engine.gfx.sprite.Atlas;
 import vulc.engine.input.KeyBindings;
 
 public class Player extends Mob {
 
 	public Player() {
-		xr = 48;
-		yr = 96;
+		xr = 32;
+		yr = 64;
 
 		x = 60;
 		y = 150;
@@ -30,8 +32,15 @@ public class Player extends Mob {
 
 	public void render(Screen screen) {
 		screen.renderSprite(new IntBitmap(xr * 2, yr * 2, 0xff0000), x - xr, y - yr);
-//		screen.renderSprite(Atlas.getSprite(Atlas.PLAYER, 0, 0, 0, 0),
-//		                    x - xr, y - yr);
+
+		Bitmap<Integer> sprite;
+		if(moveAnimation == 0) {
+			sprite = Atlas.getSprite(Atlas.PLAYER, dir * 2, 0, 2, 4);
+		} else {
+			sprite = Atlas.getSprite(Atlas.PLAYER, dir * 2, (1 + ((moveAnimation / 10) & 1)) * 4, 2, 4);
+		}
+
+		screen.renderSprite(sprite, x - xr, y - yr);
 		screen.writeOffset(dir + "", 0x00ff00, x, y - 100);
 	}
 
